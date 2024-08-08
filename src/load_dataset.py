9ebd677c65488e0109ee990b9a2e28f32f8d65e7
sanbuddhacharyas <sangambuddhacharya@gmail.com>
2024-08-08 15:35:18 -0700

    Problem in ComBat

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from glob import glob
 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
@@ -34,25 +34,41 @@ def create_correlation_matrix(X_correl:pd.DataFrame) -> None:
     os.makedirs(OUTPUT_PATH, exist_ok=True)
     plt.savefig(f'{OUTPUT_PATH}/feature_correlation_matrix.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
      
+def select_normalizer(standardize_type):
+    if standardize_type   == 'mean_std':scaler = StandardScaler() 
+    elif standardize_type == 'min_max': scaler = MinMaxScaler()
+    elif standardize_type == 'robust':  scaler = RobustScaler()
 
-def load_dataset(dataset_path=None, normalization=True, normalize_blanks=False, standardize_type='min_max') -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    return scaler
+
+def load_dataset(dataset_path=None, normalization=True, normalize_blanks=False, standardize_type='', eval_correl_matrix=False, split=True) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     if dataset_path==None: dataset_path = DATASET_PATH
 
-    datasets = sorted([f"{i}/extracted_features.xlsx" for i in glob(f'{dataset_path}/*')])
-   
-    df  = [pd.read_excel(dataset) for dataset in datasets]
-    df  = pd.concat(df)
+    if 'ML1_ML2'in os.path.basename(dataset_path):
+        datasets = sorted([f"{i}/extracted_features.xlsx" for i in glob(f'{dataset_path}/*')])
+    
+        df  = [pd.read_excel(dataset) for dataset in datasets]
+        df  = pd.concat(df)
+    
+    else: df = pd.read_excel(f'{dataset_path}/extracted_features.xlsx')
     
     X   = df[["peak area", "peak curvature", "peak V", "vcenter", "PH", "signal_mean", "signal_std", \
                                 "dS_dV_max_peak", "dS_dV_min_peak", "dS_dV_peak_diff", "dS_dV_max_V", "dS_dV_min_V", "dS_dV_area"]]
     y   = df['file'].apply(lambda x: int(x.split('_')[-2].replace('cbz','')))
 
+    
     X.rename(columns={"PH": 'univariate, max(S)', 'signal_std':'univariate, std(S)', 'signal_mean':'univariate, mean(S)', 'peak area':'univariate, area(S)', \
                         'dS_dV_area':'univariate, area(dS/dV)', 'dS_dV_max_peak':'univariate, max(dS/dV)', 'dS_dV_min_peak':'univariate, min(dS/dV)',\
                     'dS_dV_peak_diff':'univariate, max(dS/dV) - min(dS/dV)', \
                     'peak V':'univariate, V_max(S)', 'dS_dV_max_V':'univariate, V_max(dS/dV)', 'dS_dV_min_V':'univariate, V_min(dS/dV)',\
         }, inplace = True)
 
+    if not(split): 
+        if not(normalization): return X, y
+        else:
+            scaler = select_normalizer(standardize_type)
+            X_ = scaler.fit_transform(X)
+            return pd.DataFrame(X_, columns=X.columns), y
 
     # Split the total dataset into training (60%) and testing (40%) dataset
     X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=0.4, shuffle=True, random_state=20, stratify=y)
@@ -60,7 +76,7 @@ def load_dataset(dataset_path=None, normalization=True, normalize_blanks=False, 
     scaler = None
     if normalization:
         # Initialize the StandardScaler
-        scaler = StandardScaler() if standardize_type == 'mean_std' else MinMaxScaler()
+        scaler = select_normalizer(standardize_type)
 
         # Fit the scaler to only the blank of the training dataset
         if (normalize_blanks and standardize_type=='mean_std'): scaler.fit(X_train[y_train==0].copy())
@@ -81,7 +97,8 @@ def load_dataset(dataset_path=None, normalization=True, normalize_blanks=False, 
     else:  X_train, X_test = pd.DataFrame(X_train, columns=X.columns), pd.DataFrame(X_test, columns=X.columns)
 
     # Generate Feature Correlation heat map
-    create_correlation_matrix(X_train.copy())
+    if eval_correl_matrix:
+        create_correlation_matrix(X_train.copy())
     
     print("######Data Distribution:#########")
     print("Training", find_concentration_distribution(y_train))
