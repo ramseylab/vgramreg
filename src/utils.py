be416a9880c923148db90e7e25c79a21412e417c
sanbuddhacharyas <sangambuddhacharya@gmail.com>
2024-08-12 11:08:14 -0700

    Adds t-SNE and PCA plot generator in the utils section

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
+import os
+
 import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def find_adj_score(N: int, P: int, R_2: float) -> float:
     return (1 - (1 - R_2)*(N - 1)/(N - P - 1))
@@ -36,3 +43,52 @@ def calculate_y_LOD(X_train, y_train):
 
     return 2.636369 * S * SD # We got the constant value from the -qt(0.01/2, 83) there number of blanks = 84 and we are using k-1 degree 84 -1 = 83
 
+
+def tsen_pca_viz(data, batch_labels, labels, filename=''):
+    tsne        = TSNE(n_components=2, random_state=42)
+    tsne_result = tsne.fit_transform(data)
+    
+    # Convert t-SNE result to a DataFrame for easier plotting
+    tsne_df          = pd.DataFrame(data=tsne_result, columns=['t-SNE1', 't-SNE2'])
+    tsne_df['Batch'] = batch_labels
+    tsne_df['labels'] = labels
+
+    os.makedirs('batch_effect', exist_ok=True)
+    
+    # Plot t-SNE
+
+    fig, axs = plt.subplots(4, 1, figsize=(15, 12))
+    
+    sns.scatterplot(x='t-SNE1', y='t-SNE2', hue='Batch', style='labels', data=tsne_df, palette='deep', markers=['o', 's', '^'], s=20, ax=axs[0])
+    axs[0].set_title('t-SNE: Batch Effect Visualization')
+    axs[0].legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    
+
+    
+    # Perform PCA
+    pca        = PCA(n_components=2)
+    pca_result = pca.fit_transform(data)
+    
+    # Convert PCA result to a DataFrame for easier plotting
+    pca_df = pd.DataFrame(data=pca_result, columns=['PC1', 'PC2'])
+    pca_df['Batch']  = batch_labels
+    pca_df['labels'] = labels
+    
+    # Plot PCA
+    sns.scatterplot(x='PC1', y='PC2', hue='Batch', style='labels', data=pca_df, palette='deep', markers=['o', 's', '^'], s=20, ax=axs[1])
+    axs[1].set_title('PCA: Batch Effect Visualization')
+    axs[1].legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    plt.subplot(4,1,3)
+    sns.kdeplot(data=pca_df, x='PC1', hue='Batch', fill=True, common_norm=False, palette='deep', alpha=0.5)
+    # axs[2].set_title('Density Plot')
+    axs[2].set_xlabel('PCA Component Value')
+    axs[2].set_ylabel('Density')
+
+    plt.subplot(4,1,4)
+    sns.kdeplot(data=pca_df, x='PC2', hue='Batch', fill=True, common_norm=False, palette='deep', alpha=0.5)
+    # axs[3].set_title('Density Plot')
+    axs[3].set_xlabel('PCA Component Value')
+    axs[3].set_ylabel('Density')
+
+    plt.savefig(f'batch_effect/{filename}.png', dpi=300)
